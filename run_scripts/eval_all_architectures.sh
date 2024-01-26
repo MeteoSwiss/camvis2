f4f5b53c153f169010e651b16837429f1d02117c
tha-santacruz <nicola.santacroce@protonmail.com>
2024-01-26 09:32:45 +0100

    added license headers

--- run_scripts/eval_all_architectures.sh
+++ run_scripts/eval_all_architectures.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2024 MeteoSwiss, contributors listed in AUTHORS
+#
+# Distributed under the terms of the BSD 3-Clause License.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 #!/bin/bash
 
 counter = 1
