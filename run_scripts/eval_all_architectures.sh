e195ec2ef8e14c20c7f86e409b1b88a8594b7bdf
tha-santacruz <nicola.santacroce@protonmail.com>
2024-01-29 10:46:43 +0100

    fixed counter

--- run_scripts/eval_all_architectures.sh
+++ run_scripts/eval_all_architectures.sh
@@ -6,23 +6,23 @@
 
 #!/bin/bash
 
-counter = 1
 for VAL_FOLD in {1..9}; do
+    COUNTER=0
     for USE_MLP in mlp nomlp; do
         for SHARE_WEIGHTS in sharing nosharing; do
             for ALIGN_FEATURES in alignment noalignment; do
+                ((COUNTER ++))
                 python model/run.py \
                     --RUN_MODE eval \
                     --VAL_FOLD $VAL_FOLD \
                     --LOAD_CHECKPOINT \
                     --MODEL_NAME "${USE_MLP}_${SHARE_WEIGHTS}_${ALIGN_FEATURES}_fold_${VAL_FOLD}" \
                     --EVAL_SCORES_FNAME all_architectures \
-                    --EVAL_GROUP type_${counter} \
+                    --EVAL_GROUP type_${COUNTER} \
                     $(if [ "$ALIGN_FEATURES" == "alignment" ]; then echo "--ALIGN_FEATURES"; fi) \
-                    $(if [ "$SHARE_WEIGHTS" == "sharing" ]; then echo "--ALIGN_FEATURES"; fi) \
+                    $(if [ "$SHARE_WEIGHTS" == "sharing" ]; then echo "--SHARE_WEIGHTS"; fi) \
                     $(if [ "$USE_MLP" == "mlp" ]; then echo "--USE_MLP"; fi) \
-
-                ((counter ++))
+                
             done
         done
     done
